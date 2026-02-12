6e6bf07c42c8064836ee495cbae396178deceb1c
Ramon Smeekens <verboddo@gmail.com>
2026-02-12 16:56:02 +0100

    Initial Commit

---
+++ ClipCook/ClipCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipCookApp.swift
+//  ClipCook
+//
+//  Created by Ramon Smeekens on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClipCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipCookTests/ClipCookTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClipCookTests.swift
+//  ClipCookTests
+//
+//  Created by Ramon Smeekens on 12/02/2026.
+//
+
+import Testing
+
+struct ClipCookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
